0eb5d18cf87b799c46ab74ffeec96fdda89776a6
Miula925 <111400400+Miula925@users.noreply.github.com>
2023-12-03 22:54:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,334 @@
+let close = document.getElementById("testeFechar")
+let close2 = document.getElementById("testeFechar2")
+let close3 = document.getElementById("testeFechar3")
+let close4 = document.getElementById("testeFechar4")
+let close5 = document.getElementById("testeFechar5")
+let close6 = document.getElementById("testeFechar6")
+let close7 = document.getElementById("testeFechar7")
+let close8 = document.getElementById("testeFechar8")
+let close9 = document.getElementById("testeFechar9")
+let close10 = document.getElementById("testeFechar10")
+
+// modal 1
+close.addEventListener('click', () => {
+    abrir1().style.display = "none"
+
+})
+
+function abrir1() {
+    let esco1 = document.getElementById("escondido")
+
+    if (esco1.style.display === "none")
+    esco1.style.display = "block"
+else {
+    esco1.style.display = "none"
+}
+}
+
+
+
+// modal 2
+close2.addEventListener('click', () => {
+    abrir2().style.display = "none"
+})
+
+function abrir2() {
+    let esco2 = document.getElementById("escondido2")
+
+    if (esco2.style.display === "none")
+        esco2.style.display = "block"
+    else {
+        esco2.style.display = "none"
+    }
+}
+
+
+
+// modal 3
+close3.addEventListener('click', () => {
+    abrir3().style.display = "none"
+})
+
+function abrir3() {
+    let esco3 = document.getElementById("escondido3")
+
+    if (esco3.style.display === "none")
+        esco3.style.display = "block"
+    else {
+        esco3.style.display = "none"
+    }
+}
+
+
+
+// modal 4
+close4.addEventListener('click', () => {
+    abrir4().style.display = "none"
+})
+
+function abrir4() {
+    let esco4 = document.getElementById("escondido4")
+
+    if (esco4.style.display === "none")
+        esco4.style.display = "block"
+    else {
+        esco4.style.display = "none"
+    }
+}
+
+
+
+// modal5
+close5.addEventListener('click', () => {
+    abrir5().style.display = "none"
+})
+
+function abrir5() {
+    let esco5 = document.getElementById("escondido5")
+
+    if (esco5.style.display === "none")
+        esco5.style.display = "block"
+    else {
+        esco5.style.display = "none"
+    }
+}
+
+
+
+// modal 6
+close6.addEventListener('click', () => {
+    abrir6().style.display = "none"
+})
+
+function abrir6() {
+    let esco6 = document.getElementById("escondido6")
+
+    if (esco6.style.display === "none")
+        esco6.style.display = "block"
+    else {
+        esco6.style.display = "none"
+    }
+}
+
+
+
+// modal 7
+close7.addEventListener('click', () => {
+    abrir7().style.display = "none"
+})
+
+function abrir7() {
+    let esco7 = document.getElementById("escondido7")
+
+    if (esco7.style.display === "none")
+        esco7.style.display = "block"
+    else {
+        esco7.style.display = "none"
+    }
+
+    
+}
+
+
+
+// modal 8
+close8.addEventListener('click', () => {
+    abrir8().style.display = "none"
+})
+
+function abrir8() {
+    let esco8 = document.getElementById("escondido8")
+
+    if (esco8.style.display === "none")
+        esco8.style.display = "block"
+    else {
+        esco8.style.display = "none"
+    }
+}
+
+
+
+// modal 
+close9.addEventListener('click', () => {
+    abrir9().style.display = "none"
+})
+
+function abrir9() {
+    let esco9 = document.getElementById("escondido9")
+
+    if (esco9.style.display === "none")
+        esco9.style.display = "block"
+    else {
+        esco9.style.display = "none"
+    }
+}
+
+
+
+// modal 10
+close10.addEventListener('click', () => {
+    abrir10().style.display = "none"
+})
+
+function abrir10() {
+    let esco10 = document.getElementById("escondido10")
+
+    if (esco10.style.display === "none")
+        esco10.style.display = "block"
+    else {
+        esco10.style.display = "none"
+    }
+}
+
+
+// ===================================================================
+
+// audio
+
+// audio 1
+const imagem = document.getElementById('imagem');
+const audio = document.getElementById('audio');
+
+imagem.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+
+
+// audio 2
+const imagem2 = document.getElementById('imagem2');
+const audio2 = document.getElementById('audio2');
+
+imagem2.addEventListener('click', () => {
+    if (audio2.paused) {
+        audio2.play();
+    } else {
+        audio2.pause();
+        audio2.currentTime = 0;
+    }
+});
+
+// audio 3
+const imagem3 = document.getElementById('imagem3');
+const audio3 = document.getElementById('audio3');
+
+imagem3.addEventListener('click', () => {
+    if (audio3.paused) {
+        audio3.play();
+    } else {
+        audio3.pause();
+        audio3.currentTime = 0;
+    }
+});
+
+// audio 4
+const imagem4 = document.getElementById('imagem4');
+const audio4 = document.getElementById('audio4');
+
+imagem4.addEventListener('click', () => {
+    if (audio4.paused) {
+        audio4.play();
+    } else {
+        audio4.pause();
+        audio4.currentTime = 0;
+    }
+});
+
+
+// audio 5
+const imagem5 = document.getElementById('imagem5');
+const audio5 = document.getElementById('audio5');
+
+imagem5.addEventListener('click', () => {
+    if (audio5.paused) {
+        audio5.play();
+    } else {
+        audio5.pause();
+        audio5.currentTime = 0;
+    }
+});
+
+
+// audio 6
+const imagem6 = document.getElementById('imagem6');
+const audio6 = document.getElementById('audio6');
+
+imagem6.addEventListener('click', () => {
+    if (audio6.paused) {
+        audio6.play();
+    } else {
+        audio6.pause();
+        audio6.currentTime = 0;
+    }
+});
+
+// audio 7
+const imagem7 = document.getElementById('imagem7');
+const audio7 = document.getElementById('audio7');
+
+imagem7.addEventListener('click', () => {
+    if (audio7.paused) {
+        audio7.play();
+    } else {
+        audio7.pause();
+        audio7.currentTime = 0;
+    }
+});
+
+// audio 8
+const imagem8 = document.getElementById('imagem8');
+const audio8 = document.getElementById('audio8');
+
+imagem8.addEventListener('click', () => {
+    if (audio8.paused) {
+        audio8.play();
+    } else {
+        audio8.pause();
+        audio8.currentTime = 0;
+    }
+});
+
+// audio 9
+const imagem9 = document.getElementById('imagem9');
+const audio9 = document.getElementById('audio9');
+
+imagem9.addEventListener('click', () => {
+    if (audio9.paused) {
+        audio9.play();
+    } else {
+        audio9.pause();
+        audio9.currentTime = 0;
+    }
+});
+
+
+// audio 10
+const imagem10 = document.getElementById('imagem10');
+const audio10 = document.getElementById('audio10');
+
+imagem10.addEventListener('click', () => {
+    if (audio10.paused) {
+        audio10.play();
+    } else {
+        audio10.pause();
+        audio10.currentTime = 0;
+    }
+});
+
+// ===================================================================
+
+// texto portugues
+function abrirTexto(){
+    let texto = document.getElementById("textoescondido")
+    if(texto.style.display === "none")
+        texto.style.display = "block"
+    else{
+        texto.style.display = "none"
+    }
+}
+
+
